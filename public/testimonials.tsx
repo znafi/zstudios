c74e45d45be5c0c3c4e22225b53744029be4b938
znafi <znafi@ualberta.ca>
2025-08-07 19:36:42 -0600

    update: change hero section subtext to 'Your A-Z. Engineered by Z.'

---
+++ public/testimonials.tsx
@@ -0,0 +1,6 @@
+// Testimonial images as base64 strings
+export const testimonialImages = {
+  john: "/images/john-peterson.jpg",
+  omar: "/images/omar-khan.jpg",
+  emily: "/images/emily-roberts.jpg"
+};
